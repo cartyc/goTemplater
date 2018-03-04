00c1c000ef9cfa0f132eb09cb8249c20b9fe1eef
Chris Carty <chriscartydev@gmail.com>
2018-03-03 22:08:15 -0500

    first commit

---
+++ example/example.go
@@ -0,0 +1,9 @@
+package main
+
+import "gotemplater"
+
+func main() {
+
+	gotemplater.LoadTemplates("static")
+
+}
--- templater.go
+++ templater.go
@@ -1,100 +1,45 @@
-package gotemplater
+package main
 
 import (
-	"github.com/oxtoacart/bpool"
-	"path/filepath"
-	"html/template"
-	"log"
-	"net/http"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
-/*
-Adapted From
-https://hackernoon.com/golang-template-2-template-composition-and-how-to-organize-template-files-4cb40bcdf8f6
-*/
+func main() {
 
-var templates map[string]*template.Template
-var bufpool *bpool.BufferPool
-
-
-type TemplateConfig struct {
-	TemplateLayoutPath  string
-	TemplateIncludePath string
-}
-
-var mainTmpl = `{{define "main" }} {{ template "base" . }} {{ end }}`
-
-var templateConfig TemplateConfig
-
-func LoadConfiguration(main string, subtemplates string) {
-	templateConfig.TemplateLayoutPath = subtemplates
-	templateConfig.TemplateIncludePath = main
-}
-
-func LoadStatic(dir string){
-
-
-	http.Handle("/img/", http.FileServer(http.Dir(dir)))
-	http.Handle("/css/", http.FileServer(http.Dir(dir)))
-	http.Handle("/js/", http.FileServer(http.Dir(dir)))
+	GetTemplates("templates")
 
 }
 
-func LoadTemplates() {
-	if templates == nil {
-		templates = make(map[string]*template.Template)
-	}
+func GetTemplates(path string) {
 
+	/*
+		What Do I Want here?
+		Loop over dir and find folders containing templates
+		Create Templates
+		Folder structure
+		main
+		-- group
+		-- *.html
+		- group
+		-- *.html
+		*.html
+	*/
 
-	layoutFiles, err := filepath.Glob(templateConfig.TemplateLayoutPath + "*.html")
+	var files []string
+	fmt.Println(path)
+
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		fmt.Println(info.IsDir())
+		files = append(files, path)
+		return nil
+	})
+
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	includeFiles, err := filepath.Glob(templateConfig.TemplateIncludePath + "*.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mainTemplate := template.New("main")
-
-	mainTemplate, err = mainTemplate.Parse(mainTmpl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, file := range includeFiles {
-		fileName := filepath.Base(file)
-		files := append(layoutFiles, file)
-		templates[fileName], err = mainTemplate.Clone()
-		if err != nil {
-			log.Fatal(err)
-		}
-		templates[fileName] = template.Must(templates[fileName].ParseFiles(files...))
-	}
-
-	log.Println("templates loading successful")
-
-	bufpool = bpool.NewBufferPool(64)
-	log.Println("buffer allocation successful")
-}
-
-func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
-	tmpl, ok := templates[name]
-	if !ok {
-		http.Error(w, fmt.Sprintf("The template %s does not exist.", name),
-			http.StatusInternalServerError)
-	}
-
-
-	buf := bufpool.Get()
-	defer bufpool.Put(buf)
-
-	err := tmpl.Execute(buf, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	buf.WriteTo(w)
+	fmt.Println(files)
 }
